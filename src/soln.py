c5369cc32872b31d3227b29823ce6d67b0f6ae5a
esmilc <esmilcanet0711@gmail.com>
2026-04-01 12:16:59 -0400

    added finding the actual value

---
+++ src/soln.py
@@ -0,0 +1,22 @@
+
+
+
+
+def highest_val_subseq(a,b,vals):
+    len_a, len_b = len(a), len(b)
+
+    dp = [[0] * (len_a + 1)] * (len_b + 1)
+    
+    #i -> b, j -> a
+    for i in range(1, len_b + 1):
+        print(dp)
+        for j in range(1, len_a + 1):
+            if a[j-1] == b[i-1]:
+                dp[i][j] = dp[i-1][j-1] + vals[a[j-1]]
+            else:
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+    print(dp[-1][-1])
+
+
+
+highest_val_subseq("aacb", "caab", {'c':5, 'a':2, 'b':4})
