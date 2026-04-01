30164698fa2bfce341751f8c88d12ac069259731
esmilc <esmilcanet0711@gmail.com>
2026-04-01 12:17:19 -0400

    forgot to hit save

--- src/soln.py
+++ src/soln.py
@@ -9,7 +9,7 @@ def highest_val_subseq(a,b,vals):
     
     #i -> b, j -> a
     for i in range(1, len_b + 1):
-        print(dp)
+        # print(dp)
         for j in range(1, len_a + 1):
             if a[j-1] == b[i-1]:
                 dp[i][j] = dp[i-1][j-1] + vals[a[j-1]]
